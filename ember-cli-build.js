71b092dfaaf4665dbbda605bac2716271996fb4d
Arnis Vuskans <ampatspell@gmail.com>
2018-04-19 17:31:50 +0300

    debug option

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     'ember-cli-remark': {
+      debug: true,
       paths: {
         'content': 'app/content',
         'markdown': 'app/markdown'
