1edcf8154c79c927033f45a13758b0da7254a71b
ampatspell <ampatspell@gmail.com>
2018-06-17 02:37:14 +0300

    dummy

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,12 +1,15 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
+let first = true;
+
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
 
   didTransition() {
-    if(window.scrollTo) {
+    if(window.scrollTo && !first) {
+      first = false;
       window.scrollTo(0, 0);
     }
     this._super(...arguments);
